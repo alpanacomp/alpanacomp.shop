aa46cfc015f984ab02070aedabc725dafb431a94
alpanacomp <alpanacomp1969@gmail.com>
2025-05-28 04:57:43 +0000

    test change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="text-center p-4 bg-gray-100 dark:bg-gray-800 text-sm text-gray-600 dark:text-gray-300">
-      © {new Date().getFullYear()} Alpana Computers. All rights reserved.
+      © {new Date().getFullYear()} Alpana Computers. Al rights reserved.
     </footer>
   );
 }
